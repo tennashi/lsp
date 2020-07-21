613118aad5f853778db900f32d6c7caf77cb28d9
tennashi <tennashio@gmail.com>
2020-07-22 01:32:39 +0900

    fix example

--- _example/main.go
+++ _example/main.go
@@ -12,7 +12,7 @@ type Handler struct{}
 
 func (Handler) OnInitialize(ctx context.Context, c *lsp.Conn, p lsp.InitializeParams) (lsp.InitializeResult, error) {
 	return lsp.InitializeResult{
-		ServerInfo: lsp.ServerInfo{
+		ServerInfo: &lsp.ServerInfo{
 			Version: "v0.0.1",
 			Name:    "sample-ls",
 		},
